575e1dd97f2056d58914fe803d53cebb5e3a67d7
Elie898 <elie_008@hotmail.com>
2025-03-06 11:20:40 +0100

    DI + connection string

--- PublisherData/PubContext.cs
+++ PublisherData/PubContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using PublisherDomain;
 
 namespace PublisherData;
@@ -11,16 +10,11 @@ public class PubContext : DbContext
     public DbSet<Artist> Artists { get; set; }
     public DbSet<Cover> Covers { get; set; }
     public DbSet<AuthorByArtist> AuthorsByArtist { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public PubContext(DbContextOptions<PubContext>options) : base(options)
     {
-        optionsBuilder.UseSqlServer(
-          "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubDatabase"
-        ).LogTo(Console.WriteLine,
-                new[] { DbLoggerCategory.Database.Command.Name },
-                LogLevel.Information)
-        .EnableSensitiveDataLogging();
+        
     }
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<AuthorByArtist>().HasNoKey()
